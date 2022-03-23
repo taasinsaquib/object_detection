6806bf4c8d2b2e284463e5ce962320d3f9bf85c6
Taasin <taasin.saquib@gmail.com>
2022-03-23 15:34:30 -0700

    bug in iou midpoint

--- iou.py
+++ iou.py
@@ -3,7 +3,7 @@
 
 import torch
 
-def IoU(boxes_pred, boxes_labels, box_format=""):
+def iou(boxes_pred, boxes_labels, box_format=""):
 	"""
 	Calculates intersection over union for pairs of bounding boxes
 
@@ -25,13 +25,13 @@ def IoU(boxes_pred, boxes_labels, box_format=""):
 	if box_format == 'midpoint':
 		pred_x1 = boxes_pred[:, 0] - boxes_pred[:, 2] / 2
 		pred_y1 = boxes_pred[:, 1] - boxes_pred[:, 3] / 2
-		pred_x2 = boxes_pred[:, 2] + boxes_pred[:, 2] / 2
-		pred_y2 = boxes_pred[:, 3] + boxes_pred[:, 3] / 2
+		pred_x2 = boxes_pred[:, 0] + boxes_pred[:, 2] / 2
+		pred_y2 = boxes_pred[:, 1] + boxes_pred[:, 3] / 2
 
 		labels_x1 = boxes_labels[:, 0] - boxes_labels[:, 2] / 2
 		labels_y1 = boxes_labels[:, 1] - boxes_labels[:, 3] / 2
-		labels_x2 = boxes_labels[:, 2] + boxes_labels[:, 2] / 2
-		labels_y2 = boxes_labels[:, 3] + boxes_labels[:, 3] / 2
+		labels_x2 = boxes_labels[:, 0] + boxes_labels[:, 2] / 2
+		labels_y2 = boxes_labels[:, 1] + boxes_labels[:, 3] / 2
 
 	else:
 		pred_x1 = boxes_pred[:, 0]
@@ -46,12 +46,17 @@ def IoU(boxes_pred, boxes_labels, box_format=""):
 
 	# Intersection ************************************************************
 
+	print(pred_x1, pred_y1, pred_x2, pred_y2)
+	print(labels_x1, labels_y1, labels_x2, labels_y2)
+
 	# Get intersection points of each pair of boxes
 	x1 = torch.max(pred_x1, labels_x1)
 	y1 = torch.max(pred_y1, labels_y1)
 
 	x2 = torch.min(pred_x2, labels_x2)
 	y2 = torch.min(pred_y2, labels_y2)
+
+	print(x1, y1, x2, y2)
 
 	# If boxes don't intersect, either (x2 - x1) or (y2 - y1) will be negative
 	# we just set the minimum to zero as the area would be zero
@@ -75,17 +80,29 @@ def main():
 
 	# using ... instead of : would allow one row tensors to not need to be surrounded by braces
 
-	# no intersection
-	pred   = torch.Tensor([[1, 1, 2, 2]])
-	labels = torch.Tensor([[3, 3, 4, 4]])
-	iou = IoU(pred, labels)
-	print(iou)
+	# # no intersection
+	# pred   = torch.Tensor([[1, 1, 2, 2]])
+	# labels = torch.Tensor([[3, 3, 4, 4]])
+	# i = iou(pred, labels)
+	# print(i)
 
-	# intersection
-	pred   = torch.Tensor([[1, 1, 4, 4]])
-	labels = torch.Tensor([[0, 0, 3, 3]])
-	iou = IoU(pred, labels)
-	print(iou)
+	# # intersection
+	# pred   = torch.Tensor([[1, 1, 4, 4]])
+	# labels = torch.Tensor([[0, 0, 3, 3]])
+	# i = iou(pred, labels)
+	# print(i)
+
+	# pred   = torch.Tensor([[0.8, 0.1, 0.2, 0.2]])
+	# labels = torch.Tensor([[0.9, 0.2, 0.2, 0.2]])
+	# i = iou(pred, labels, 'midpoint')
+	# assert(i - 1/7 < 1e-3)
+
+	# caught bug in midpoint
+	pred   = torch.Tensor([[0.5, 0.45, 0.4, 0.5]])
+	labels = torch.Tensor([[0.5, 0.5, 0.2, 0.4]])
+	i = iou(pred, labels, 'midpoint')
+	# assert(i - 3/13 < 1e-3)
+	print(i)
 
 if __name__ == "__main__":
 	main()
